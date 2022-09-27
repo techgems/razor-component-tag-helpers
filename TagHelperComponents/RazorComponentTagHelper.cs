8ee78a9432323f368babf537a0b82f58a2d44536
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2022-09-27 10:37:46 -0600

    Add XML comments.

--- TagHelperComponents/RazorComponentTagHelper.cs
+++ TagHelperComponents/RazorComponentTagHelper.cs
@@ -9,27 +9,48 @@ using System.Threading.Tasks;
 
 namespace TechGems.TagHelperComponents;
 
+/// <summary>
+/// The base class for a component tag helper that leverages a razor partial view. Sends itself as the view model for the razor template.
+/// </summary>
 public abstract class RazorComponentTagHelper : TagHelper
 {
     private readonly string? _razorViewRoute;
 
+    /// <summary>
+    /// Creates the tag helper with a razor view route using default route.
+    /// </summary>
     public RazorComponentTagHelper()
     {
 
     }
 
+    /// <summary>
+    /// Creates the tag helper with a razor view route that overrides the default route.
+    /// </summary>
+    /// <param name="razorViewRoute"></param>
     public RazorComponentTagHelper(string razorViewRoute)
     {
         _razorViewRoute = razorViewRoute;
     }
 
+    /// <summary>
+    /// The View Context necessary to get the Html Helper that renders the partial views.
+    /// </summary>
     [HtmlAttributeNotBound]
     [ViewContext]
     public ViewContext? ViewContext { get; set; }
 
+    /// <summary>
+    /// Child content for rendering in the razor template.
+    /// </summary>
     [HtmlAttributeNotBound]
     public TagHelperContent? ChildContent { get; set; }
 
+    /// <summary>
+    /// Gets the Html Helper from the View Context. Used for rendering partial views.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
     private IHtmlHelper GetHtmlHelper()
     {
         if (ViewContext is null)
@@ -47,7 +68,7 @@ public abstract class RazorComponentTagHelper : TagHelper
 
 
     /// <summary>
-    /// By default we will render the razor template based on the default route.
+    /// Default ProcessAsync method. Will render the default razor view if a route is not provided in the base class.
     /// </summary>
     /// <param name="context"></param>
     /// <param name="output"></param>
@@ -70,6 +91,13 @@ public abstract class RazorComponentTagHelper : TagHelper
         }
     }
 
+    /// <summary>
+    /// Uses the HtmlHelper to render the partial view. Defaults tag name to null and adds child content if there was any.
+    /// Will send the child class as the view model for the partial view.
+    /// </summary>
+    /// <param name="viewRoute"></param>
+    /// <param name="output"></param>
+    /// <returns></returns>
     protected async Task RenderPartialView(string viewRoute, TagHelperOutput output) 
     {
         var childContent = await output.GetChildContentAsync();
@@ -87,6 +115,11 @@ public abstract class RazorComponentTagHelper : TagHelper
         output.TagName = null;
     }
 
+    /// <summary>
+    /// Render the razor view from the default route.
+    /// </summary>
+    /// <param name="output"></param>
+    /// <returns></returns>
     protected async Task RenderPartialView(TagHelperOutput output)
     {
         string defaultViewPath = $"~/TagHelpers/{GetType().Namespace!.Split('.').Last()}/Default.cshtml";
